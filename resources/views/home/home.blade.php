3666a64e39b6b1fdebeea9a77364e47039c068e2
Ganjar Hadiatna <ganjarhadiatna.gh@gmail.com>
2018-04-23 23:25:49 +0700

    fixed ui login & register

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,7 +5,9 @@
 <div class="frame-guess">
 	<div class="block bdr-bottom">
 		<div class="logo" style="background-image: url('{{ asset('/img/CocaCode/2.png') }}')"></div>
-		<div class="ttl ctn-main-font ctn-small ctn-center padding-10px">You have a Box Now.</div>
+		<div class="ttl ctn-main-font ctn-small ctn-center padding-10px">
+			Share What You Can Do.
+		</div>
 	</div>
 	<div class="banner bdr-bottom">
 		<div class="cover">
